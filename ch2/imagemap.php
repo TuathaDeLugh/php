9d712c4f4c1e2d74a6fe54fc8bd3accaf339157e
Umang Sailor <ursailor@gmail.com>
2023-03-06 19:48:04 +0530

    imagemap

---
+++ ch2/imagemap.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="https://variety.com/wp-content/uploads/2023/01/Netflix-One-Piece.png?w=676" alt="Workplace" usemap="#workmap">
+
+<map name="workmap">
+  <area shape="rect" coords="34,44,270,350" alt="rock" href="rock.html">
+  <area shape="circle" coords="337,300,44" alt="lufy" href="lufy.html">
+</map>
+</body>
+</html>
