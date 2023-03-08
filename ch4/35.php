b4d08d9811f09c9ac21216c55d9bee6d6be7d461
Umang Sailor <ursailor@gmail.com>
2023-03-08 21:12:11 +0530

    exception

---
+++ ch4/35.php
@@ -0,0 +1,22 @@
+<?php
+$filename="Umang.txt";
+fopen($filename,"r")
+or die("Unable to Open $filename");
+
+function num($num) {
+    if($num%2==0) {
+      throw new Exception("Given no is Even(this is throw exception)");
+    }
+    return true;
+  }
+  
+  try {
+    num(2);
+    echo "Given no is Even(this is Try Block)";
+  }
+  
+  //catch exception
+  catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+  }
+?>
