3c4945d79d1cef7f8033a9b2775bcefaa0669f26
Umang Sailor <ursailor@gmail.com>
2023-04-11 22:38:20 +0530

    added all table query

--- ch5/table.sql
+++ ch5/table.sql
@@ -1,6 +1,6 @@
 -- for old40 && 42
 CREATE TABLE cust (cust_no int NOT NULL AUTO_INCREMENT PRIMARY KEY,cust_name varchar(255),item_purchase varchar(255),mob_no varchar(255));
--- for 
+-- for 44
 CREATE TABLE emp (emp_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,emp_name varchar(255),des varchar(255),selary int(7));
 INSERT INTO emp VALUES(NULL,'Umang','Head',40000);
 INSERT INTO emp VALUES(NULL,'Utsav','manager',30000);
@@ -8,5 +8,5 @@ INSERT INTO emp VALUES(NULL,'Parth','employee',20000);
 INSERT INTO emp VALUES(NULL,'Karan','employee',20000);
 INSERT INTO emp VALUES(NULL,'Neel','employee',22000);
 INSERT INTO emp VALUES(NULL,'Deep','employee',18000);
---for 1
+--for 41
 create table student(enroll varchar(12),name varchar(30),dob varchar(8),dept varchar(20));
